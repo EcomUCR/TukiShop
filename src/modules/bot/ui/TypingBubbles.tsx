9c96ab880cdd2da17b3a8bec157df614d241cb06
JohnHito <johnjairo988@gmail.com>
2025-11-02 02:01:16 -0600

    Chatbot ajustes

--- src/modules/bot/ui/TypingBubbles.tsx
+++ src/modules/bot/ui/TypingBubbles.tsx
@@ -2,9 +2,9 @@ import { motion } from "framer-motion";
 
 export default function TypingBubbles() {
   return (
-    <div className="flex items-center gap-1 bg-blue-50 p-3 rounded-2xl w-fit shadow-sm">
+    <div className="flex items-center gap-1 bg-gray-100 p-3 rounded-2xl w-fit shadow-sm">
       <motion.span
-        className="w-2.5 h-2.5 bg-blue-600 rounded-full"
+        className="w-2.5 h-2.5 bg-gradient-to-br from-contrast-main to-contrast-secondary rounded-full"
         animate={{
           y: [0, -6, 0],
         }}
@@ -16,7 +16,7 @@ export default function TypingBubbles() {
         }}
       />
       <motion.span
-        className="w-2.5 h-2.5 bg-blue-600 rounded-full"
+        className="w-2.5 h-2.5 bg-gradient-to-br from-contrast-main to-contrast-secondary rounded-full"
         animate={{
           y: [0, -6, 0],
         }}
@@ -29,7 +29,7 @@ export default function TypingBubbles() {
         }}
       />
       <motion.span
-        className="w-2.5 h-2.5 bg-blue-600 rounded-full"
+        className="w-2.5 h-2.5 bg-gradient-to-br from-contrast-main to-contrast-secondary rounded-full"
         animate={{
           y: [0, -6, 0],
         }}
