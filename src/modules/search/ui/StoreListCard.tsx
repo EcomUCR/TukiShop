9aaa61dec2fc783313dbbcee3cf784929cd9ad47
RealNavi <joseraulhmjr91220@gmail.com>
2025-11-03 23:03:03 -0600

    StoreListCard

--- src/modules/search/ui/StoreListCard.tsx
+++ src/modules/search/ui/StoreListCard.tsx
@@ -16,15 +16,15 @@ interface StoreProps {
 
 export default function StoreListCard({ store }: StoreProps) {
   return (
-    <div className="relative flex flex-col border border-white/20 bg-gradient-to-br from-contrast-main/50 via-contrast-secondary/50 to-main/50 backdrop-blur-lg rounded-2xl shadow-lg overflow-hidden w-full max-w-[30rem] mx-auto font-quicksand transition-all duration-300 sm:w-[35rem] min-h-[28rem] sm:min-h-[26rem]">
-      <div className="relative h-24 sm:h-28 md:h-32 bg-gradient-to-r from-main via-contrast-main to-contrast-secondary">
+    <div className="relative flex flex-col border border-white/20 bg-gradient-to-br from-contrast-main/60 via-contrast-secondary/50 to-main/50 backdrop-blur-lg rounded-2xl shadow-lg overflow-hidden w-full max-w-[30rem] mx-auto font-quicksand transition-all duration-300 sm:w-[35rem] min-h-[25rem] sm:min-h-[26rem]">
+      <div className="relative h-30 sm:h-28 md:h-40 ">
         <img
           src={
             store.banner ??
             "https://res.cloudinary.com/dpbghs8ep/image/upload/v1761410400/BannerNoSubido_avlp5v.png"
           }
           alt={`${store.name} banner`}
-          className="h-full w-full object-cover"
+          className="h-full w-full object-cover p-2 rounded-2xl"
         />
       </div>
       <div className="absolute top-16 left-5 sm:top-25 sm:left-6">
@@ -34,10 +34,10 @@ export default function StoreListCard({ store }: StoreProps) {
             "https://res.cloudinary.com/dpbghs8ep/image/upload/v1761412207/imagenNoSubida_dymbb7.png"
           }
           alt={`${store.name} logo`}
-          className="w-20 h-20 sm:w-18 sm:h-18 md:w-20 md:h-20 rounded-full border-4 bg-white border-white object-contain shadow-md"
+          className="w-25 h-25 sm:w-18 sm:h-18 md:w-30 md:h-30 rounded-full border-4 bg-white/70 border-white object-contain shadow-md"
         />
       </div>
-      <div className="flex relative flex-col items-center pt-14 sm:pt-15 pb-6 px-4 text-center flex-1">
+      <div className="flex relative flex-col items-center pt-14 sm:pt-5 pb-6 px-4 text-center flex-1">
         <p className="absolute top-2 right-3 bg-white/50 text-xs text-main px-3 py-1 rounded-full">
           {store.category?.trim() || "Sin categoría"}
         </p>
